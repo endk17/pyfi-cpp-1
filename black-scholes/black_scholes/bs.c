82a694d64c8d15566f2bdfee54fc97c26baa080e
Enda Kelly <kelly.enda@gmail.com>
2022-11-16 08:13:04 +0000

    define pi

---
+++ black-scholes/black_scholes/bs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <Python.h>
+
+// when C compilers don't define PI 
+#ifndef PI 
+#define PI 3.141592653589793238462643
+#endif
+ 
+#ifndef PI 
+const double PI=3.141592653589793238462643;
+#endif 
